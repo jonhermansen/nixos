639197f598791927bc112c00c58a56ee3fe77957
Jon Hermansen <jon@jh86.org>
2025-08-03 20:34:06 -0400

    Enable zfs, more packages, fix x11 config

--- configuration.nix
+++ configuration.nix
@@ -9,8 +9,19 @@
   boot.kernelPackages = pkgs.linuxPackages_6_15;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
+  boot.supportedFilesystems = [ "zfs" ];
   environment.systemPackages = with pkgs; [
+    (wrapOBS {
+      plugins = with obs-studio-plugins; [
+        wlrobs
+        obs-backgroundremoval
+        obs-pipewire-audio-capture
+      ];
+    })
+    brave
+    cargo
     colmena
+    curl
     dmenu
     emacs-nox
     fastfetch
@@ -21,8 +32,10 @@
     i3status
     i3-volume
     librewolf
+    mangohud
     sway
     xorg.xdpyinfo
+    zig
   ];
   hardware.graphics.enable = true;
   hardware.nvidia = {
@@ -35,16 +48,27 @@
   };
   networking.firewall.allowedTCPPorts = [ 22 ];
   networking.firewall.enable = true;
+  networking.hostId = "9461e5a3";
   networking.hostName = "desktop";
   networking.networkmanager.enable = true;
   nix.settings.experimental-features = [ "flakes" "nix-command" ];
   nixpkgs.config.allowUnfree = true;
   services.gnome.gnome-keyring.enable = true;
   services.openssh.enable = true;
-  services.xserver.displayManager.startx.enable = true;
-  services.xserver.enable = true;
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver = {
+    displayManager.startx.enable = true;
+    enable = true;
+    screenSection = ''
+    Option "Stereo" "0"
+    Option "nvidiaXineramaInfoOrder" "DP-2"
+    Option "metamodes" "1920x1080_144 +0+0 {AllowGSYNCCompatible=On}"
+    Option "SLI" "Off"
+    Option "MultiGPU" "Off"
+    Option "BaseMosaic" "off"
+    '';
+    windowManager.i3.enable = true;
+    videoDrivers = [ "nvidia" ];
+  };
   system.stateVersion = "25.11";
   time.timeZone = "US/Eastern";
   users.users.user = {
@@ -52,4 +76,3 @@
     extraGroups = [ "input" "video" "wheel" ];
   };
 }
-
